5c9e8e8fecb3b1ddbda5cc892a2ce84e2707de13
coderpro1234-2 <94269830+coderpro1234-2@users.noreply.github.com>
2021-12-08 12:03:38 +1100

    Add files via upload

---
+++ scratch-extensions/Operators-2.0.js
@@ -0,0 +1,125 @@
+class Operators2 {
+  getInfo() {
+    return {
+      id: "Operators2",
+      name: 'Operators 2.0',
+      blocks: [
+        {
+          opcode: 'plus',
+          blockType: Scratch.BlockType.REPORTER,
+          text: '[LEFT] + [RIGHT]',
+          arguments: {
+            LEFT: {
+              type: Scratch.ArgumentType.NUMBER,
+            },
+            RIGHT: {
+              type: Scratch.ArgumentType.NUMBER,
+            }
+          }
+        },
+        {
+          opcode: 'minus',
+          blockType: Scratch.BlockType.REPORTER,
+          text: '[LEFT] - [RIGHT]',
+          arguments: {
+            LEFT: {
+              type: Scratch.ArgumentType.NUMBER,
+            },
+            RIGHT: {
+              type: Scratch.ArgumentType.NUMBER,
+            }
+          }
+        },
+        {
+          opcode: 'times',
+          blockType: Scratch.BlockType.REPORTER,
+          text: '[LEFT] * [RIGHT]',
+          arguments: {
+            LEFT: {
+              type: Scratch.ArgumentType.NUMBER,
+            },
+            RIGHT: {
+              type: Scratch.ArgumentType.NUMBER,
+            }
+          }
+        },
+        {
+          opcode: 'divide',
+          blockType: Scratch.BlockType.REPORTER,
+          text: '[LEFT] / [RIGHT]',
+          arguments: {
+            LEFT: {
+              type: Scratch.ArgumentType.NUMBER,
+            },
+            RIGHT: {
+              type: Scratch.ArgumentType.NUMBER,
+            }
+          }
+        },
+        {
+          opcode: 'powerof',
+          blockType: Scratch.BlockType.REPORTER,
+          text: '[LEFT] ** [RIGHT]',
+          arguments: {
+            LEFT: {
+              type: Scratch.ArgumentType.NUMBER,
+            },
+            RIGHT: {
+              type: Scratch.ArgumentType.NUMBER,
+            }
+          }
+        },
+        {
+           opcode: 'and',
+           blockType: Scratch.BlockType.REPORTER,
+           text: '[LEFT] & [RIGHT]',
+           arguments: {
+             LEFT: {
+               type: Scratch.ArgumentType.NUMBER,
+             },
+             RIGHT: {
+               type: Scratch.ArgumentType.NUMBER,
+            }
+          }
+        },
+         {
+           opcode: 'xor',
+           blockType: Scratch.BlockType.REPORTER,
+           text: '[LEFT] ^ [RIGHT]',
+           arguments: {
+             LEFT: {
+               type: Scratch.ArgumentType.NUMBER,
+             },
+             RIGHT: {
+               type: Scratch.ArgumentType.NUMBER,
+            }
+          }
+        }
+      ]
+    };
+  }
+  
+  plus({ LEFT, RIGHT }) {
+    return LEFT + RIGHT;
+  }
+  minus({ LEFT, RIGHT }) {
+    return LEFT - RIGHT;
+   }
+  times({ LEFT, RIGHT }) {
+    return LEFT * RIGHT;
+   }
+  divide({ LEFT, RIGHT }) {
+    return LEFT / RIGHT;
+   }
+  powerof({ LEFT, RIGHT }) {
+    return LEFT ** RIGHT;
+    }
+  and({ LEFT, RIGHT }) {
+    return LEFT & RIGHT;
+      }
+  xor({ LEFT, RIGHT }) {
+    return LEFT ^ RIGHT;
+  }
+}
+
+Scratch.extensions.register(new Operators2());
