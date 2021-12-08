b8b32a6fdc397317efedf46a2087171b80296118
coderpro1234-2 <94269830+coderpro1234-2@users.noreply.github.com>
2021-12-08 17:54:07 +1100

    Update comment.js

--- scratch-extensions/comment.js
+++ scratch-extensions/comment.js
@@ -1,1 +1,22 @@
+class comment {
+  getInfo() {
+    return {
+      id: "comment",
+      name: 'Comment block',
+      blocks: [
+        {
+          opcode: 'comment',
+          blockType: Scratch.BlockType.COMMAND,
+          text: '// [CMT] //',
+          arguments: {
+            CMT: {
+              type: Scratch.ArgumentType.NUMBER,
+            }
+          }
+        }
+      ]
+    };
+  }
+}
 
+Scratch.extensions.register(new comment());
