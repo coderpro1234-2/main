c17a27c2a98aa2b101c43fbc1cc4437ee4c84a0c
coderpro1234-2 <94269830+coderpro1234-2@users.noreply.github.com>
2021-12-08 18:12:41 +1100

    Update and rename comment.js to placeholder.js

---
+++ scratch-extensions/placeholder.js
@@ -0,0 +1,44 @@
+class comment {
+  getInfo() {
+    return {
+      id: "comment",
+      name: 'Comment block',
+      blocks: [
+        {
+          opcode: 'comment',
+          blockType: Scratch.BlockType.COMMAND,
+          text: '// [CMT] //',
+          arguments: {
+            CMT: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'comment'
+            }
+          }
+        },
+        {
+          opcode: 'placecom',
+          blockType: Scratch.BlockType.COMMAND,
+          text: 'Placeholder',
+            }
+          }
+        },
+        {
+          opcode: 'placeboo',
+          blockType: Scratch.BlockType.BOOLEAN,
+          text: 'Placeholder',
+            }
+          }
+        }
+        {
+          opcode: 'placerep',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'Placeholder',
+            }
+          }
+        }
+      ]
+    };
+  }
+}
+
+Scratch.extensions.register(new comment());
