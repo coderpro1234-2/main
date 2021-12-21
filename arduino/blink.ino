c9920db41455116595f8e81163e6a8903604e10d
coderpro1234-2 <94269830+coderpro1234-2@users.noreply.github.com>
2021-12-21 17:25:16 +1100

    Add files via upload

---
+++ arduino/blink.ino
@@ -0,0 +1,10 @@
+int led = 13;
+void setup() {                
+  pinMode(led, OUTPUT);     
+}
+void loop() {
+  digitalWrite(led, HIGH);   
+  delay(50); 
+  digitalWrite(led, LOW);   
+  delay(50);  
+}
