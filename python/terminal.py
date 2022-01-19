4e8007a1b29b13196031a49c0af9c4d28a0e2ab0
coderpro1234-2 <94269830+coderpro1234-2@users.noreply.github.com>
2022-01-19 11:19:35 +1100

    Add files via upload

---
+++ python/terminal.py
@@ -0,0 +1,70 @@
+print("This is Terminal v1.3")
+print("Type 'Help' for commands")
+while True:
+    text = input("")
+    text = text.lower()
+    if text == "print":
+        text = input("print what? ")
+        print(text)
+    elif text == "add":
+        num1 = input("add what? ")
+        num2 = input("to what? ")
+        text = num1 + num2
+        num =(str(text).isnumeric())
+        if num == True:
+            text = int(num1) + int(num2)
+            print(text)
+        else:
+            print("NaN")
+    elif text == "minus":
+        num1 = input("minus from what? ")
+        num2 = input("minus what? ")
+        text = num1 + num2
+        num =(str(text).isnumeric())
+        if num == True:
+            text = int(num1) - int(num2)
+            print(text)
+        else:
+            print("NaN")
+    elif text == "multiply":
+        num1 = input("multiply what? ")
+        num2 = input("by what? ")
+        text = num1 + num2
+        num =(str(text).isnumeric())
+        if num == True:
+            text = int(num1) * int(num2)
+            print(text)
+        else:
+            print("NaN")
+    elif text == "div":
+        num1 = input("divide what? ")
+        num2 = input("into what? ")
+        text = num1 + num2
+        num =(str(text).isnumeric())
+        if num == True:
+            text = int(num1) / int(num2)
+            print(text)
+        else:
+            print("NaN")
+    elif text == "power":
+        num1 = input("what? ")
+        num2 = input("to the power of ")
+        text = num1 + num2
+        num =(str(text).isnumeric())
+        if num == True:
+            text = int(num1) ** int(num2)
+            print(text)
+        else:
+            print("NaN")
+    elif text == "exit":
+        print("Good Bye!")
+        exit()
+    elif text == "help":
+        print('''Commands:
+Print
+Add
+Minus
+Multiply
+Div
+Exit
+''')
